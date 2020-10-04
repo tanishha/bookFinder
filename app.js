626130db2dff0149e7b4c137a8ce61f524c7ba2a
Tanisha Chaudhary <tanishach203@gmail.com>
2020-10-05 00:41:25 +0545

    heroku

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*',(req,res)=>{
-        res.sendFile(__dirname,'client','build','index.html');  //relative path
+        res.sendFile(path.join(__dirname,'client','build','index.html'));  //relative path
     })
 }
 
